fc9747f954304c95908ce995a029e16e09f115fa
Vincent-zhangS <vzhang2022@gmail.com>
2025-11-11 18:13:42 -0500

    added post

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+const userRouter = require('./routes/users');
+const postRouter = require('./routes/posts');
 const app = express();
 app.set('view engine', 'ejs');
+app.use('/users', userRouter);
+app.use('/posts',postRouter);
 
 app.get('/', (req, res) => {
   console.log('Here');
