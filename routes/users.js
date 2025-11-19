15d711cdd2e26f06e7f3610c8cd849635641966d
Vincent-zhangS <vzhang2022@gmail.com>
2025-11-19 16:14:41 -0500

    added the stuff from class and remained the 2 js so server works

---
+++ routes/users.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.render('users/list', { users: users })
+});
+
+router.get('/new', (req, res) => {
+    res.render('users/new', {firstName: "Please enter a username"});
+});
+
+// router.post('/new', (req, res)=> {
+//     res.render('users/new', {firstName: "Please enter name"});
+// });
+
+router.post("/", (req, res) => {
+    res.send("User Created!");
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const gender = req.body.gender;
+    const age = req.body.age;
+    const isValid = firstName != "" && lastName != "" && age != 0;
+
+    if (isValid) {
+        console.log(`Adding user ${firstName}`);
+        users.push({ 
+            firstName: firstName, 
+            lastName: lastName,
+            gender: gender,
+            age: age,
+        });
+        console.log("New set of users: " + `${users}`)
+    }
+    else {
+        console.log("Error adding user!");
+        res.render("users/new", { firstName: firstName });
+    }
+});
+
+router.route("/:id").get((req, res) => {
+    res.send(`Getting User data: ${req.params.id}`);
+}).delete((req, res) => {
+    res.send(`Deleting user with ID: ${req.params.id}`);
+}).put((req, res) => {
+    res.send(`Updating user with ID: ${req.params.id}`);
+});
+
+const users = [
+  {
+    firstName: "Vincent",
+    lastName: "Zhang",
+    gender: "Male",
+    age: 21,
+  },
+  {
+    firstName: "George",
+    lastName: "Salayka",
+    gender: "Male",
+    age: 40,
+  }
+];
+router.param('id', (req, res, next, id) => {
+    console.log(`Accessing user #${id}`);
+    next();
+});
+
+module.exports = router;
